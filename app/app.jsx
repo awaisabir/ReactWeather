1bf75824366b1cf86c28a83f39192581f5120ed0
awaisabir <awaisabirqureshi@gmail.com>
2017-05-03 01:51:58 -0400

    main jsx file

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+var MainComponent = require('Main');
+var WeatherComponent = require('Weather');
+var AboutComponent = require('About');
+var ExamplesComponent = require('Examples');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={MainComponent}>
+      <Route path="about" component={AboutComponent}/>
+      <Route path="examples" component={ExamplesComponent}/>
+      <IndexRoute component={WeatherComponent}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
